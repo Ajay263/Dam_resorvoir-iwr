e36ae7db3e0e6fb9d3b9c2bc49d94553054e6b2d
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-06-19 05:31:17 +0200

    Updated entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,6 @@ export AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT=${AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT}
 airflow db upgrade
 
 airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
-# "$_AIRFLOW_WWW_USER_USERNAME" -p "$_AIRFLOW_WWW_USER_PASSWORD"
+#  "$_AIRFLOW_WWW_USER_USERNAME" -p "$_AIRFLOW_WWW_USER_PASSWORD"
 
 airflow webserver
